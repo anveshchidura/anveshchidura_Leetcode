2b018ebc8f32806dacc36f612dabf4a0ca85b7fb
Anvesh chidura <31155837+anveshchidura@users.noreply.github.com>
2023-09-07 19:49:46 -0400

    Time: 3 ms (29.44%) | Memory: 6.5 MB (56.01%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rec_func(int m,int n,int i,int j,vector<vector<int>> &dp){
+        if(i >= m || j >= n){
+            return 0;
+        }
+        if(i == m - 1 && j == n-1){
+            return 1;
+        }
+        if(dp[i][j] != -1){return dp[i][j];}
+        else{
+        int left = rec_func(m,n,i+1,j,dp);
+        int right = rec_func(m,n,i,j+1,dp);
+        return dp[i][j] = left + right;
+        }
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n, -1));
+        int answer = rec_func(m,n,0,0,dp);
+        return answer;
+    }
+};
